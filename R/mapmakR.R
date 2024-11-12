494e3562230e290abb11d2e0885264e7903f74c0
sagesteppe <reedbenkendorf27@gmail.com>
2024-11-12 12:40:11 -0600

    mapmaker optional layers

--- R/mapmakR.R
+++ R/mapmakR.R
@@ -8,7 +8,8 @@
 #' @param cities Boolean, whether to draw cities or not, Defaults to TRUE.
 #' @param landscape Boolean, whether to draw the map in a landscape orientation or not. Defaults to TRUE
 #' @param caption Character string, text for a caption. It's best to mention any published product related to the data set.
-#' Defaults to omitting any caption
+#' Defaults to omitting any caption, except for the data sources (if omernik and cities are used),
+#'  and coordinate reference system information.
 #' @param filetype Character string, defaults to 'pdf' for saving a pdf page for distribution with data,
 #' but 'png' (or any other format supported by ?ggsave) can be used to create
 #' a map for embedding in a publication or poster.
@@ -16,10 +17,16 @@ mapmakR <- function(x, species, outdir, ecoregions, landscape, caption, filetype
 
   if(missing(species))(stop('Species Name Not supplied.'))
   if(missing(outdir)){outdir = getwd()}
-  if(missing(caption)){caption = NULL}
   if(missing(landscape)){landscape = TRUE}
+  if(missing(ecoregions)){ecoregions = TRUE}
   if(missing(filetype)){filetype = 'pdf'}
   fname <- paste0(file.path(outdir, gsub(' ', '_', species)), '_STZmap.', filetype)
+
+  if(missing(cities)){cities <- TRUE}
+  if(cities == TRUE){
+    cities.sf <- file.path(
+      system.file(package ='eSTZwritR'), 'extdata', 'Carto_cities.gpkg')
+    }
 
   # Buffer the map so that the species only doesn't occupy the entire region.
   extent <- buffeR(x)
@@ -41,7 +48,6 @@ mapmakR <- function(x, species, outdir, ecoregions, landscape, caption, filetype
   p <- ggplot() +
     ggplot2::geom_sf(data = x, aes(fill = factor(zone)), color = NA, inherit.aes = TRUE) +
     ggplot2::geom_sf(data = states, fill = NA, lwd = 0.25, color = 'black', alpha = 0.5) +
-    ggplot2::geom_sf(data = omernik, fill = NA, lty = 3, color = 'grey30') +
     ggplot2::geom_sf(data = countries, fill = NA, lwd = 1, color = 'black') +
     ggplot2::coord_sf(
       xlim = extent[c(1,3)],
@@ -49,7 +55,6 @@ mapmakR <- function(x, species, outdir, ecoregions, landscape, caption, filetype
       expand = F) +
 
     ggplot2::labs(x = NULL, y = NULL, fill = 'Zone',
-         caption = caption,
          title = paste0('*', species, '*'),
          subtitle = 'Seed Transfer Zones') +
     ggplot2::theme(
@@ -64,6 +69,50 @@ mapmakR <- function(x, species, outdir, ecoregions, landscape, caption, filetype
                            pad_x = unit(0.25, "in"), pad_y = unit(0.25, "in"),
                            height = unit(0.35, "in"), width = unit(0.35, "in"))
 
+  #determine the correct caption format for the data.
+  if(exists(caption)){
+    if(ecoregions == TRUE & cities == FALSE){
+      caption <- paste0(caption, '\nOmernik Ecoregions data courtesy of US EPA.')
+      } else if(ecoregions == FALSE & cities == TRUE){
+        caption <- paste0(caption, '\nCities data courtesy of Simplemaps.com')
+        } else if(ecoregions == TRUE & cities == TRUE){
+          caption <- paste0(caption, '\nOmernik Ecoregions data courtesy of US EPA.\nCities data courtesy of Simplemaps.com')
+      }
+    } else {
+    if(ecoregions == TRUE & cities == FALSE){
+      caption <- paste0('Omernik Ecoregions data courtesy of US EPA.')
+      } else if(ecoregions == FALSE & cities == TRUE){
+        caption <- paste0('Cities data courtesy of Simplemaps.com')
+        } else if(ecoregions == TRUE & cities == TRUE){
+          caption <- paste0('Omernik Ecoregions data courtesy of US EPA.\nCities data courtesy of Simplemaps.com')
+        }
+  }
+
+  # determine whether to add cities and ecoregions to the plot.
+    if(ecoregions == TRUE & cities == FALSE){
+      p <- p +
+        ggplot2::geom_sf(data = omernik, fill = NA, lty = 3, color = 'grey30') +
+        ggplot2::labs(caption)
+
+    } else if(ecoregions == FALSE & cities == TRUE){
+    p <- p +
+      ggplot2::geom_sf(data = cities.sf) +
+      ggplot2::geom_sf_text(data = cities.sf, aes(label = City)) +
+      ggplot2::labs(caption)
+
+    } else if(ecoregions == TRUE & cities == TRUE){
+    p <- p +
+      ggplot2::geom_sf(data = omernik, fill = NA, lty = 3, color = 'grey30') +
+      ggplot2::geom_sf(data = cities.sf) +
+      ggplot2::geom_sf_text(data = cities.sf, aes(label = City)) +
+      ggplot2::labs(caption)
+
+    } else {
+    p <- p +
+      ggplot2::labs(caption)
+  }
+
+# save file to location
   if(landscape == TRUE){
     ggsave(filename = fname,
            plot = p, dpi = 300, height = 8.5, width = 11, units = 'in')
